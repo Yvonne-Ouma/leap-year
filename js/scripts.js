31436e83e3321953f5cc6403db0bb9bd3ddd7a52
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-07-13 10:31:29 +0300

    initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
 $(document).ready(function() {
-  $("form#leap-year").submit(function(event){
-    event.preventDefault();
-    var year = parseInt($("input#year").val());
-    var result = leapYear(year);
-    $("#result").text(result);
+    $("form#leap-year").submit(function(event) {
+      event.preventDefault();
+      var year = parseInt($("input#year").val());
+      var result = leapYear(year);
+      $("#result").text(result);
+    });
   });
-});
-var leapYear = function(year) {
+
+  var leapYear = function(year) {
     return false;
-};
+  };
+
+  var leapYear = function(year) {
+    if (year % 4 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  };

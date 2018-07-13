e820d1c95c88583fda4db783c07e62f1dac89767
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-07-13 10:51:16 +0300

    modified my js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//  business logic
 $(document).ready(function() {
     $("form#leap-year").submit(function(event) {
       event.preventDefault();
@@ -18,3 +19,40 @@ $(document).ready(function() {
       return false;
     }
   };
+  var leapYear = function(year) {
+    if (year % 100 === 0) {
+      return false;
+    } else if (year % 4 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  var leapYear = function(year) {
+    if ((year % 4 === 0) && (year % 100 !== 0)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  // user interface logic
+  $(document).ready(function() {
+    $("form#leap-year").submit(function(event) {
+      event.preventDefault();
+      var year = parseInt($("input#year").val());
+      var result = leapYear(year);
+
+      $(".year").text(year);
+
+      if (!result) {                 // same as writing if (result === false)
+        $(".not").text("not");
+      } else {
+        $(".not").text("");
+      }
+
+      $("#result").show();
+    });
+  });
+  
